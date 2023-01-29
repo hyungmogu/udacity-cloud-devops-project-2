654a4071947100ac6c3987c22dd8ed2fc311bf11
Hyungmo Gu <guhyungm7@gmail.com>
2023-01-29 12:33:45 -0700

    fix: Fixing work for the part 'Server spec' #20

--- project_starter/create.sh
+++ project_starter/create.sh
@@ -1,1 +1,7 @@
-aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+if [ -z "$4" ]
+  then
+    aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+  else
+    aws cloudformation wait stack-create-complete --stack-name $4;
+    aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1;
+fi
